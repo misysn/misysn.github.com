3d925aaf855626b4b1008df457986ae500142dc5
misysn <49383171+misysn@users.noreply.github.com>
2023-03-09 17:48:00 +0800

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "10234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "city": "CN101280101",
+      "key": "7925652631b649dd9197c53b2b3acdfe"
+    }
+  }
